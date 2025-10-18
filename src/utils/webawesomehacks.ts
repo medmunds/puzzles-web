bd6f96b7649ff27e38549ace5227b2f428e2fa72
Mike Edmunds <medmunds@gmail.com>
2025-10-18 15:52:12 -0700

    Stop suppressing Web Awesome change-in-update warnings
    Reduces initial bundle size by ~20%

--- src/utils/webawesomehacks.ts
+++ src/utils/webawesomehacks.ts
@@ -1,30 +1,6 @@
 import WaButton from "@awesome.me/webawesome/dist/components/button/button.js";
-import WaCheckbox from "@awesome.me/webawesome/dist/components/checkbox/checkbox.js";
-import WaInput from "@awesome.me/webawesome/dist/components/input/input.js";
-import WaRadio from "@awesome.me/webawesome/dist/components/radio/radio.js";
-import WaRadioGroup from "@awesome.me/webawesome/dist/components/radio-group/radio-group.js";
 import WaScroller from "@awesome.me/webawesome/dist/components/scroller/scroller.js";
-import WaSelect from "@awesome.me/webawesome/dist/components/select/select.js";
-import WaSlider from "@awesome.me/webawesome/dist/components/slider/slider.js";
-import WaTextarea from "@awesome.me/webawesome/dist/components/textarea/textarea.js";
 import { css } from "lit";
-
-/**
- * Several Web Awesome components trigger Lit's change-in-update warning.
- * Disable that warning from those components until fixed.
- *
- * https://github.com/shoelace-style/webawesome/issues/1269
- */
-function disableWaChangedInUpdateWarnings() {
-  WaButton.disableWarning?.("change-in-update");
-  WaCheckbox.disableWarning?.("change-in-update");
-  WaInput.disableWarning?.("change-in-update");
-  WaRadio.disableWarning?.("change-in-update");
-  WaRadioGroup.disableWarning?.("change-in-update");
-  WaSelect.disableWarning?.("change-in-update");
-  WaSlider.disableWarning?.("change-in-update");
-  WaTextarea.disableWarning?.("change-in-update");
-}
 
 /**
  * Fix wa-button[with-caret] width when only content is a wa-icon.
@@ -72,7 +48,6 @@ function fixWaScrollerFocusIndication() {
 }
 
 export function installWebAwesomeHacks() {
-  disableWaChangedInUpdateWarnings();
   fixWaButtonIconWithCaret();
   fixWaScrollerFocusIndication();
 }
