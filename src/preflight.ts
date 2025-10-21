c1720c23cfa9bb3ad99f77d31cc5fdce6910781c
Mike Edmunds <medmunds@gmail.com>
2025-10-20 19:37:32 -0700

    Preflight: cleanup, prepare for async checks

--- src/preflight.ts
+++ src/preflight.ts
@@ -1,25 +1,34 @@
 /**
  * Browser capabilities to check before letting the user into the site.
- * (We tend to develop with Baseline 2023 as the minimum.)
+ * (We generally expect Baseline 2023 and don't use polyfills.)
  */
 const preflightChecks: {
-  [feature: string]: boolean | (() => boolean) | (() => Promise<boolean>);
+  // Checks return true for passed, false for failed
+  [feature: string]: () => boolean;
 } = {
-  "CSS.supports": typeof "CSS" !== "undefined" && "supports" in CSS, // ~2015; needed below
-  ResizeObserver: typeof "ResizeObserver" !== "undefined", // Baseline 2020
-  // TODO: top-level await (supported if modules are) -- Baseline 2021
-  //   May not be relevant
+  "CSS.supports": () => typeof CSS.supports === "function",
+  ResizeObserver: () => typeof "ResizeObserver" !== "undefined", // Baseline 2020
   "Canvas.transferControlToOffscreen": // Baseline 2023
-    "transferControlToOffscreen" in document.createElement("canvas"),
-  // TODO: WorkerGlobalContext.requestAnimationFrame -- Baseline 2023
-  //   Only real way to know is fire up a worker (with an async check).
-  //   Is there any main thread functionality that likely released together?
+    () => "transferControlToOffscreen" in document.createElement("canvas"),
   "CustomStateSet un-prefixed states": checkCustomStateSetUnprefixedStates,
 
   // CSS -- all Baseline 2023
-  "CSS nested selectors": CSS.supports("selector(& .foo)"),
-  "CSS container queries": CSS.supports("container-type", "size"),
-};
+  "CSS nested selectors": () => CSS.supports("selector(& .foo)"),
+  "CSS container queries": () => CSS.supports("container-type", "size"),
+} as const;
+
+const asyncPreflightChecks: {
+  // async checks can indicate pass/fail either by returning true/false
+  // or by throwing for fail, returning nothing for pass
+  // biome-ignore lint/suspicious/noConfusingVoidType: try `() => Promise.reject()`
+  [feature: string]: () => Promise<boolean | void>;
+} = {
+  // TODO: top-level await (supported if modules are) -- Baseline 2021
+  //   May not be relevant
+  // TODO: WorkerGlobalContext.requestAnimationFrame -- Baseline 2023
+  //   Only real way to know is fire up a worker (with an async check).
+  //   Is there any main thread functionality that likely released together?
+} as const;
 
 function checkCustomStateSetUnprefixedStates() {
   // Baseline 2024?
@@ -50,13 +59,15 @@ export async function runPreflightChecks(runAll = false) {
   if (typeof Object.entries !== "function") {
     return ["Object.entries"]; // ~2017
   }
+  if (typeof Promise !== "function") {
+    return ["Promise"];
+  }
 
-  // TODO: run async checks in parallel
   const failed: string[] = [];
   for (const [feature, check] of Object.entries(preflightChecks)) {
-    let passed = false;
+    let passed: boolean | Promise<void>;
     try {
-      passed = typeof check === "function" ? await check() : check;
+      passed = check();
     } catch {
       passed = false;
     }
@@ -67,6 +78,41 @@ export async function runPreflightChecks(runAll = false) {
       }
     }
   }
+
+  if (runAll || failed.length === 0) {
+    // Run async checks concurrently (even when not runAll).
+    // Resolve each to `undefined` for passed, feature name for failed.
+    const asyncResults = await Promise.allSettled(
+      Object.entries(asyncPreflightChecks).map(
+        ([feature, check]) =>
+          new Promise<string | undefined>(
+            (resolve) =>
+              check()
+                .then(
+                  (result) =>
+                    result || result === undefined
+                      ? resolve(undefined) // passed (returned true or nothing)
+                      : resolve(feature), // failed (returned false)
+                )
+                .catch(() => resolve(feature)), // failed (rejected)
+          ),
+      ),
+    );
+    for (const result of asyncResults) {
+      if (result.status === "fulfilled" && result.value) {
+        failed.push(result.value);
+        if (!runAll) {
+          break;
+        }
+      } else if (!import.meta.env.PROD && result.status === "rejected") {
+        // This should have been converted to resolve(feature) by the logic above
+        throw new Error(
+          `Unprocessed rejection in preflight asyncResults: ${result.reason} `,
+        );
+      }
+    }
+  }
+
   return failed;
 }
 
