599bf7d203c624f7ae4b97a05d67a8cf4e62b308
Mike Edmunds <medmunds@gmail.com>
2025-12-28 12:02:02 -0800

    Fix potential head-matter issues
    Try to avoid creating corrupt DOMs or provoking browser shadow DOM bugs:
    - Switch from node.cloneNode to document.importNode
    - Protect updateHead from running after disconnect
    - Use Set to avoid duplicating existing nodes or removing added nodes
    - Restrict slotted content that can be cloned into head

--- src/head-matter.ts
+++ src/head-matter.ts
@@ -21,8 +21,8 @@ import { customElement } from "lit/decorators.js";
  */
 @customElement("head-matter")
 export class HeadMatter extends LitElement {
-  private originalNodes: HTMLElement[] = [];
-  private addedNodes: HTMLElement[] = [];
+  private originalNodes = new Set<HTMLElement>();
+  private addedNodes = new Set<HTMLElement>();
 
   render() {
     return html`<slot @slotchange=${this.handleSlotChange}></slot>`;
@@ -43,6 +43,10 @@ export class HeadMatter extends LitElement {
   }
 
   private updateHead(newChildNodes: Element[]) {
+    if (!this.isConnected) {
+      return;
+    }
+
     this.restoreHead();
 
     const queries = new Set<string>();
@@ -52,8 +56,8 @@ export class HeadMatter extends LitElement {
         const query = this.getQueryForElement(node);
         if (!queries.has(query)) {
           for (const match of document.head.querySelectorAll(query)) {
-            if (match instanceof HTMLElement) {
-              this.originalNodes.push(match);
+            if (match instanceof HTMLElement && !this.addedNodes.has(match)) {
+              this.originalNodes.add(match);
               match.remove();
             }
           }
@@ -61,8 +65,8 @@ export class HeadMatter extends LitElement {
         }
 
         // Add the new element to the head and track it.
-        const clone = node.cloneNode(true) as HTMLElement;
-        this.addedNodes.push(clone);
+        const clone = document.importNode(node, true) as HTMLElement;
+        this.addedNodes.add(clone);
         document.head.appendChild(clone);
       }
     }
@@ -75,22 +79,43 @@ export class HeadMatter extends LitElement {
     for (const node of this.addedNodes) {
       node.remove();
     }
-    this.addedNodes = [];
+    this.addedNodes.clear();
 
     for (const node of this.originalNodes) {
       document.head.appendChild(node);
     }
-    this.originalNodes = [];
+    this.originalNodes.clear();
   }
 
   private getQueryForElement(element: HTMLElement): string {
-    if (element instanceof HTMLLinkElement && element.rel) {
-      return `link[rel="${element.rel}"]`;
+    const tagName = element.tagName.toLowerCase();
+    switch (tagName) {
+      case "link":
+        if (element.hasAttribute("rel")) {
+          return `link[rel="${element.getAttribute("rel") ?? ""}"]`;
+        }
+        break;
+      case "meta":
+        // Currently only using <meta name=...>.
+        // Could add support for http-equiv, property, etc. if needed.
+        if (element.hasAttribute("name")) {
+          return `meta[name="${element.getAttribute("name") ?? ""}"]`;
+        }
+        break;
+      case "template":
+        // In theory, head-matter could support any element with an 'id'.
+        // But updateHead() currently reorders elements to the end of <head>,
+        // which could change interpretation of script, style, etc.
+        if (element.id) {
+          return `${tagName}#${element.id}`;
+        }
+        break;
+      case "base":
+      case "title":
+        // Singleton elements
+        return tagName;
     }
-    if (element instanceof HTMLMetaElement && element.name) {
-      return `meta[name="${element.name}"]`;
-    }
-    return element.tagName;
+    throw new Error(`Unsupported slotted ${tagName} in head-matter`);
   }
 }
 
